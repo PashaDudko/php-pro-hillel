e8dd039ab540f9c9b09c718540980e3b18c9d4f9
usr01 <pashaclient100_@ukr.net>
2024-02-12 18:49:35 +0200

    single-responsibility

---
+++ Product.php
@@ -0,0 +1,27 @@
+<?php
+
+class Product
+{
+    public function get(string $name): Product
+    {
+
+    }
+
+    public function set(string $name, $value): void
+    {
+
+    }
+}
+
+class EditActionsWithProduct
+{
+    public function save() {}
+    public function update() {}
+    public function delete() {}
+}
+
+class DisplayActionsWithProduct
+{
+    public function show() {}
+    public function print() {}
+}
